678778d2bf436ab843795acd3f18068af16ea738
Bhushan-chanore <chabhushan16@gmail.com>
2024-08-09 15:32:56 +0530

    Second

--- src/components/Cardpart2.jsx
+++ src/components/Cardpart2.jsx
@@ -1,29 +1,27 @@
 import React from 'react'
 
-function Cardpart2(data) {
-    const {name} = data;
+function Cardpart2({ name }) {
     return (
         <div>
-            <div className=' relative flex flex-row flex-wrap sm:w-[400px] w-[320px] bg-slateblue hover:bg-white hover:text-black text-white   rounded-[5px] py-[1.875rem] pr-[2.469rem] pl-[2.5rem]'>
+            <div className='relative flex flex-row flex-wrap sm:w-[400px] w-[320px] bg-slateblue hover:bg-white hover:text-black text-white rounded-[5px] py-[1.875rem] pr-[2.469rem] pl-[2.5rem] transition-colors duration-300'>
                 <div className="flex flex-col items-start justify-start gap-[0.319rem]">
-                    <div className="relative tracking-[0.26px] leading-[1.5rem] font-medium shrink-0 text-royalblue-100 [debug_commit:f6aba90]">
+                    <div className="relative tracking-[0.26px] leading-[1.5rem] font-medium text-royalblue-100">
                         Explore the Features
                     </div>
-                    <h1 className="m-0 relative text-[1.688rem] tracking-[-1.08px] leading-[1.806rem] font-bold font-inherit hover:text-black  shrink-0 [debug_commit:f6aba90] mq450:text-[1.375rem] mq450:leading-[1.438rem]">
-                        <p className="m-0">{name}</p>
-                        {/* <p className="m-0">{name1}</p> */}
-                    </h1>
+                    <h2 className="m-0 relative text-[1.688rem] tracking-[-1.08px] leading-[1.806rem] font-bold font-inherit hover:text-black">
+                        {name}
+                    </h2>
                     <div className='flex flex-row gap-4 mt-[5rem]'>
-
-                        <div className="flex flex-col items-start justify-start pt-[1rem] px-[0rem] pb-[0rem]">
-                            <div className="relative tracking-[0.42px] leading-[0.875rem] font-semibold inline-block min-w-[81.7px]">
+                        <button className="flex flex-col items-start justify-start pt-[1rem] px-[0rem] pb-[0rem] text-white">
+                            <span className="relative tracking-[0.42px] leading-[0.875rem] font-semibold inline-block min-w-[81.7px]">
                                 Read More
-                            </div>
-                        </div>
-                        <div className="z-[0] h-[46px] w-[46px] relative rounded-27xl box-border border-[2px] border-solid border-lavender">
-                            <div className="absolute top-[calc(50%_-_7.5px)] left-[calc(50%_-_0.5px)] bg-black w-px h-[15px]" />
-                            <div className="absolute top-[calc(50%_-_0.5px)] left-[calc(50%_-_7.5px)] bg-black w-[15px] h-px z-[1]" />
-                        </div>
+                            </span>
+                        </button>
+                        <button className="z-[0] h-[46px] w-[46px] relative rounded-27xl box-border border-[2px] border-solid border-lavender flex items-center justify-center">
+                            <div className="absolute bg-black w-px h-[15px]"></div>
+                            <div className="absolute bg-black w-[15px] h-px z-[1]"></div>
+                            <span className="sr-only">Add</span> {/* For screen readers */}
+                        </button>
                     </div>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,37 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import ScrollIndicator from '../components/ScrollIndicator'
-// import rightarrow from "../Images/right-arrow-svgrepo-com.svg"
-
 import img1 from "../Images/svg-1.svg"
-
 import Hero from '../components/Hero'
 import Cardpart2 from '../components/Cardpart2'
 import Cryptop03 from '../components/Cryptop03'
 import Cryptop04 from '../components/Cryptop04'
-// import Cryptop05 from '../components/Cryptop05'
 import Cryptop06 from '../components/Cryptop06'
 import Cryptop7 from '../components/Cryptop7'
 import Footer from '../components/Footer'
+
 function Home() {
     return (
-        <div className='landingpage h-[100vh] bg-darkblue-100 ' style={{backgroundColor:"#2434b3"}}>
-        
+        <div className='landingpage h-[100vh] bg-darkblue-100' style={{ backgroundColor: "#2434b3" }}>
             <Navbar />
             <ScrollIndicator left="5%" right="auto" ballOffset="15" />
             <ScrollIndicator left="35%" right="auto" ballOffset="30" />
             <ScrollIndicator left="auto" right="35%" ballOffset="12" />
             <ScrollIndicator left="auto" right="5%" ballOffset="20" />
 
-          
+            <div className="homy z-0 gap-[5rem] h-[100vh] flex flex-row flex-wrap justify-between items-center sm:mx-[5rem] mx-0">
+                <div className='flex flex-col'>
+                    <div className="flex flex-col items-center sm:items-start">
+                        <h1 className="sm:text-[5rem] text-[3.5rem] tracking-[-3px] leading-[4.688rem] font-bold font-inherit text-white z-[0] text-center sm:text-justify w-[320px] sm:w-[600px] mx-auto sm:mx-0">
+                            Crypto blockchain revolution is here
+                        </h1>
 
-            <div className="homy z-0 gap-[5rem] h-[100vh] flex flex-row flex-wrap justify-between  items-center sm:mx-[5rem] mx-0">
+                        <h2 className="sm:text-[1.2rem] text-[0.8rem] mt-4 leading-[1.625rem] font-medium font-inherit z-[0] text-white opacity-80 text-center sm:text-left w-[320px] sm:w-[600px]">
+                            Lorem ipsum dolor sit amet, consectetuer adipis cingdiam nonummy nibhie euismod tincidunt utlaoreet.
+                        </h2>
+                    </div>
 
-            <div className='flex flex-col '>
-
-                <div className="flex flex-col items-center sm:items-start">
-                    <h1 className="sm:text-[5rem] text-[3.5rem] tracking-[-3px] leading-[4.688rem] font-bold font-inherit text-white z-[0] text-center sm:text-justify w-[320px] sm:w-[600px] mx-auto sm:mx-0">
-                        <p className="m-0">Crypto blockchain revolution is here</p>
-                    </h1>
-
-                    <h3 className="sm:text-[1.2rem] text-[0.8rem] mt-4 leading-[1.625rem] font-medium font-inherit z-[0] text-white opacity-80 text-center sm:text-left w-[320px] sm:w-[600px]">
-                        <p className="m-0">
-                            Lorem ipsum dolor sit amet, consectetuer adipis cingdiam nonummy nibhie euismod tincidunt utlaoreet.</p>
-                    </h3>
-
-                </div>
-
-                <div className="flex flex-col items-center sm:flex-row sm:items-start gap-4">
+                    <div className="flex flex-col items-center sm:flex-row sm:items-start gap-4">
                         <button className="z-0 cursor-pointer mt-4 py-[0.675rem] pl-[1rem] bg-tomato w-[150.4px] rounded-3xs box-border overflow-hidden shrink-0 flex flex-row items-end justify-start gap-[0.606rem] border-[2px] border-solid border-tomato">
                             <div className="relative text-[0.875rem] tracking-[0.42px] leading-[0.875rem] font-semibold font-montserrat text-white text-left inline-block min-w-[81.7px]">
                                 Read More
@@ -51,6 +41,7 @@ function Home() {
                                     className="w-[26px] h-[9px] relative overflow-hidden shrink-0"
                                     alt=""
                                     src={img1}
+                                    width="26" height="9"
                                 />
                             </div>
                         </button>
@@ -63,23 +54,19 @@ function Home() {
                                     className="w-[26px] h-[9px] relative overflow-hidden shrink-0"
                                     alt=""
                                     src={img1}
+                                    width="26" height="9"
                                 />
                             </div>
                         </button>
                     </div>
+                </div>
 
-
-            </div>
-
-
-
-                <div className=" justify-center items-center hidden sm:block">
+                <div className="justify-center items-center hidden sm:block">
                     <div className="max-w-full max-h-full overflow-hidden">
                         <Hero />
                     </div>
                 </div>
             </div>
-
 
             <div className='flex relative item-center flex-row flex-wrap sm:mt-[0rem] mt-[15rem] sm:justify-evenly justify-around'>
                 <div className='mb-8'>
@@ -87,7 +74,7 @@ function Home() {
                 </div>
 
                 <div className='mb-8'>
-                    <Cardpart2 name={"Introducing the marketplace sistem"} />
+                    <Cardpart2 name={"Introducing the marketplace system"} />
                 </div>
 
                 <div className='mb-8'>
@@ -95,13 +82,11 @@ function Home() {
                 </div>
             </div>
 
-
-            <div className=' bg-darkblue-100 sm:mt-[10rem] mt-[10rem] sm:px-[2rem] px-0'>
+            <div className='bg-darkblue-100 sm:mt-[10rem] mt-[10rem] sm:px-[2rem] px-0'>
                 <Cryptop03 />
             </div>
 
-
-            <div className='p-5 mt-[5rem] '>
+            <div className='p-5 mt-[5rem]'>
                 <Cryptop04 />
             </div>
 
@@ -116,76 +101,8 @@ function Home() {
             <div className='mb-[10rem]'>
                 <Footer />
             </div>
-
         </div>
     )
 }
 
 export default Home
-
-
-
-
-{/* <div className='flex flex-row space-x-4'>
-
-<button className="z-0 cursor-pointer mt-4 py-[0.675rem]  pl-[1rem] bg-tomato w-[150.4px] rounded-3xs box-border overflow-hidden shrink-0 flex flex-row items-end justify-start gap-[0.606rem]  border-[2px] border-solid border-tomato">
-    <div className="relative text-[0.875rem] tracking-[0.42px] leading-[0.875rem] font-semibold font-montserrat text-white text-left inline-block min-w-[81.7px]">
-        Read More
-    </div>
-    <div className="flex flex-col items-start justify-end pt-[0rem] px-[0rem] pb-[0.125rem]">
-        <img
-            className="w-[26px] h-[9px] relative overflow-hidden shrink-0"
-            alt=""
-            src={img1}
-        />
-    </div>
-</button>
-<button className="z-0 cursor-pointer mt-4 py-[0.675rem]  pl-[1rem] bg-white w-[150.4px] rounded-3xs box-border overflow-hidden shrink-0 flex flex-row items-end justify-start gap-[0.606rem]  border-[2px] border-solid border-white">
-    <div className="relative text-[0.875rem] tracking-[0.42px] leading-[0.875rem] font-semibold font-montserrat text-midnightblue text-left inline-block min-w-[87.9px]">
-        Get Innovio
-    </div>
-    <div className="flex flex-col items-start justify-end pt-[0rem] px-[0rem] pb-[0.125rem] opacity-100 transition-transform duration-300 transform translate-x-0 group-hover:translate-x-[-1.5rem]">
-        <img
-            className="w-[26px] h-[9px] relative overflow-hidden shrink-0"
-            alt=""
-            src={img1}
-        />
-    </div>
-</button>
-</div> */}
-
-
-
-
-
-{/* <div className="">
-                    <h1 className=" text-[5rem] tracking-[-3px] leading-[4.688rem] font-bold font-inherit text-white z-[6]  text-justify">
-                        <p className="m-0">Crypto blockchain</p>
-                        <p className="m-0">revolution is here</p>
-                    </h1>
-
-                    <h3 className="sm:text-[1.2rem] mt-4 leading-[1.625rem] font-medium font-inherit z-[1] text-white opacity-80">
-                        <p className="m-0">
-                            Lorem ipsum dolor sit amet, consectetuer adipis cing
-                        </p>
-                        <p className="m-0">diam nonummy nibhie euismod tincidunt utlaoreet.</p>
-                    </h3>
-                    
-                </div> */}
-
-
-
-
-
-
-            //     <div className=' gap-[5rem] h-[100vh] flex flex-row flex-wrap justify-between items-center px-[5.4%]'>
-
-
-                
-
-            //     <div className=" flex justify-center items-center">
-            //         <div className="max-w-full max-h-full overflow-hidden">
-            //             <Hero />
-            //         </div>
-            //     </div>
-            // </div>
